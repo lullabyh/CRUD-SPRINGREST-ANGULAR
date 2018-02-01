f5e7a8ed6e0511e725d71b0193ac9b6880f47db2
Mariana <mariana.aparecida14@gmail.com>
2018-02-01 10:27:42 +0000

    first commit

---
+++ crud-springRest-angular/src/main/java/br/ufc/marianaass/crudspringRestangular/CrudSpringRestAngularApplication.java
@@ -0,0 +1,12 @@
+package br.ufc.marianaass.crudspringRestangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringRestAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringRestAngularApplication.class, args);
+	}
+}
